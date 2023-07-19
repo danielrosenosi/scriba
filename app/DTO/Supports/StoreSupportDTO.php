f19b8dc85bd81c403c03213c0939369c3e082cd0
danielrosenosi <devroseno@gmail.com>
2023-07-19 16:31:28 -0300

    refactor: small adjustments

--- app/DTO/Supports/StoreSupportDTO.php
+++ app/DTO/Supports/StoreSupportDTO.php
@@ -2,18 +2,19 @@
 
 namespace App\DTO\Supports;
 
+use App\Enums\SupportStatusEnum;
 use App\Http\Requests\StoreSupportRequest;
 
 class StoreSupportDTO
 {
     public function __construct(
         public string $subject,
-        public string $status,
+        public SupportStatusEnum $status,
         public string $body,
     ) {}
 
     public static function makeFromRequest(StoreSupportRequest $request): self
     {
-        return new self($request->subject, 'a', $request->body);
+        return new self($request->subject, SupportStatusEnum::A, $request->body);
     }
 }
--- app/DTO/Supports/UpdateSupportDTO.php
+++ app/DTO/Supports/UpdateSupportDTO.php
@@ -2,6 +2,7 @@
 
 namespace App\DTO\Supports;
 
+use App\Enums\SupportStatusEnum;
 use App\Http\Requests\UpdateSupportRequest;
 
 class UpdateSupportDTO
@@ -9,12 +10,12 @@ class UpdateSupportDTO
     public function __construct(
         public int $id,
         public string $subject,
-        public string $status,
+        public SupportStatusEnum $status,
         public string $body,
     ) {}
 
     public static function makeFromRequest(UpdateSupportRequest $request, int $id): self
     {
-        return new self($id, $request->subject, 'a', $request->body);
+        return new self($id, $request->subject, SupportStatusEnum::A, $request->body);
     }
 }
