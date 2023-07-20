8d6aa386c0b51a0999a5bc19d8c7eda5d49de4e5
danielrosenosi <devroseno@gmail.com>
2023-07-20 09:27:43 -0300

    feat: creating SupportApiController

---
+++ app/Http/Controllers/Api/SupportApiController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\DTO\Supports\StoreSupportDTO;
+use App\DTO\Supports\UpdateSupportDTO;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreSupportRequest;
+use App\Http\Requests\UpdateSupportRequest;
+use App\Http\Resources\SupportResource;
+use App\Services\SupportService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class SupportApiController extends Controller
+{
+    public function __construct(protected SupportService $service) {}
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $support = $this->service->paginate(
+            page: $request->get('page', 1),
+            totalPerPage: $request->get('per_page', 10),
+            filter: $request->filter
+        );
+
+        return SupportResource::collection($support->items())->additional([
+            'meta' => [
+                'total' => $support->total(),
+                'is_first_page' => $support->isFirstPage(),
+                'is_last_page' => $support->isLastPage(),
+                'current_page' => $support->currentPage(),
+                'next_page' => $support->getNumberNextPage(),
+                'previous_page' => $support->getNumberPreviousPage(),
+            ]
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSupportRequest $request): SupportResource
+    {
+        $support = $this->service->store(StoreSupportDTO::makeFromRequest($request));
+
+        return new SupportResource($support);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id): SupportResource|JsonResponse
+    {
+        if (!$support = $this->service->show($id)) {
+            return response()->json(['error' => 'Suporte não encontrado'], Response::HTTP_NOT_FOUND);
+        }
+
+        return new SupportResource($support);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSupportRequest $request, string $id): SupportResource|JsonResponse
+    {
+        $support = $this->service->update(UpdateSupportDTO::makeFromRequest($request, $id));
+
+        if (!$support) {
+            return response()->json(['error' => 'Suporte não encontrado'], Response::HTTP_NOT_FOUND);
+        }
+
+        return new SupportResource($support);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id): JsonResponse
+    {
+        if (!$this->service->show($id)) {
+            return response()->json(['error' => 'Suporte não encontrado'], Response::HTTP_NOT_FOUND);
+        }
+
+        $this->service->delete($id);
+
+        return response()->json([], Response::HTTP_NO_CONTENT);
+    }
+}
