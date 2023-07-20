4bef24f9b22ec5302fda18645f3b87933b54e49b
danielrosenosi <devroseno@gmail.com>
2023-07-20 09:27:33 -0300

    refactor: small adjustments

--- app/Http/Controllers/Admin/SupportController.php
+++ app/Http/Controllers/Admin/SupportController.php
@@ -19,7 +19,7 @@ class SupportController extends Controller
     {
         $supports = $this->service->paginate(
             page: $request->get('page', 1),
-            totalPerPage: $request->get('per_page', 1),
+            totalPerPage: $request->get('per_page', 10),
             filter: $request->filter
         );
 
