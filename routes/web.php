e6d4cde60069bd3ce1f73d9ffb8c07e3eca1a01c
danielrosenosi <devroseno@gmail.com>
2023-07-20 17:02:34 -0300

    feat: breeze install in progress

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,19 @@ Route::get('/contato', [SiteController::class, 'index'])->name('site.contact');
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
