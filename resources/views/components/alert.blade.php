18d03addddf9d6202b46bb37713866b72dc405d2
danielrosenosi <devroseno@gmail.com>
2023-07-20 16:32:32 -0300

    feat: alerts

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,7 +1,7 @@
-<div class="alert alert-danger">
-    @if ($errors->any())
+@if ($errors->any())
+    <div class="dark:bg-gray-600 border-l-4 border-red-500 text-orange-100 p-4 my-4" role="alert">
         @foreach ($errors->all() as $error)
-            {{ $error }}
+            <p>{{ $error }}</p>
         @endforeach
-    @endif
-</div>
+    </div>
+@endif
