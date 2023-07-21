1d7c077dce4d1baa4502f2e8f3046d6dd5e817f9
danielrosenosi <devroseno@gmail.com>
2023-07-21 09:42:34 -0300

    feat: implementing middleware on MVC

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\SupportController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
@@ -14,14 +15,6 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-Route::put('/supports/{id}/update', [SupportController::class, 'update'])->name('supports.update');
-Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
-Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
-Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
-Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
-Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
-Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
 
 Route::get('/contato', [SiteController::class, 'index'])->name('site.contact');
 
@@ -41,6 +34,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::put('/supports/{id}/update', [SupportController::class, 'update'])->name('supports.update');
+    Route::delete('/supports/{id}', [SupportController::class, 'destroy'])->name('supports.destroy');
+    Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
+    Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+    Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
+    Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+    Route::post('/supports', [SupportController::class, 'store'])->name('supports.store');
 });
 
 require __DIR__.'/auth.php';
