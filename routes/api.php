b7438a40ce6406d2caac141f29db77c65fac350e
danielrosenosi <devroseno@gmail.com>
2023-07-21 09:42:13 -0300

    feat: implementing middleware on api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\SupportApiController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('/supports', SupportApiController::class);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'auth']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/me', [AuthController::class, 'me']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('/supports', SupportApiController::class);
+});
