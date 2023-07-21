5ab74c759cbb98cdc642f8faea8893fff393d0ec
danielrosenosi <devroseno@gmail.com>
2023-07-21 09:41:53 -0300

    feat: creating RegisterRequest

---
+++ app/Http/Requests/Api/RegisterRequest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:6', 'max:255'],
+            'name' => ['required', 'string', 'max:255'],
+            'device_name' => ['required', 'string', 'max:255'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'O campo email é obrigatório.',
+            'email.email' => 'O campo email deve ser um endereço de e-mail válido.',
+            'email.max' => 'O campo email deve ter no máximo 255 caracteres.',
+            'email.unique' => 'O campo email já está em uso.',
+            'password.required' => 'O campo senha é obrigatório.',
+            'password.min' => 'O campo senha deve ter no mínimo 6 caracteres.',
+            'password.max' => 'O campo senha deve ter no máximo 255 caracteres.',
+            'name.required' => 'O campo nome é obrigatório.',
+            'name.string' => 'O campo nome deve ser uma string.',
+            'name.max' => 'O campo nome deve ter no máximo 255 caracteres.',
+            'device_name.required' => 'O campo device_name é obrigatório.',
+            'device_name.string' => 'O campo device_name deve ser uma string.',
+            'device_name.max' => 'O campo device_name deve ter no máximo 255 caracteres.',
+        ];
+    }
+}
